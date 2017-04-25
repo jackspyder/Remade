f6f355827ed6c495411c0662014b6ccac317e817
Jack Powell <jackspider@hotmail.com>
2017-04-25 20:12:40 +0100

    Save point before NPM and mix fuck me.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const {mix} = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.js(['resources/assets/js/app.js', 'resources/assets/js/charts.js', 'resources/assets/js/autocomplete.js'], 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
