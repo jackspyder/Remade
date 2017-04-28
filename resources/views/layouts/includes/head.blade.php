ba6a282cc531bd6f4875281ca352ed920bf8c364
Jack Powell <jackspider@hotmail.com>
2017-04-28 16:08:43 +0100

    Just getting g suite verification

--- resources/views/layouts/includes/head.blade.php
+++ resources/views/layouts/includes/head.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet"/>
 
     <!-- Scripts -->
     <script>
